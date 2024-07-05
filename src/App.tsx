19edf896206b93561e8e2a3354d6471ab71f6ce7
Gabriel Arruda <hellmatgabriel@icloud.com>
2024-07-05 14:34:24 -0300

    realizada criação crud-categoria

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,30 @@ import './App.css';
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DeletarCategoria from './components/categoria/deletarCategoria/DeletarCategoria';
+import ListaCategorias from './components/categoria/listaCategorias/ListaCategorias';
+import FormularioCategoria from './components/categoria/formularioCategoria/FormularioCategoria';
+/*hellmat gabriel*/ 
+
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+<BrowserRouter>
+        <Navbar />
+        <div className='min-h-[90vh]'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/categorias" element={<ListaCategorias />} />
+          <Route path="/cadastrarCategoria" element={<FormularioCategoria />} />
+          <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+          <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+        </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
 );
 }
